9431983a79a6c860ea594d10c2e94a237c1f1970
Tom Demagny <tom.demagny@laposte.net>
2024-04-16 23:45:55 +0200

    automatique push

--- controller.py
+++ controller.py
@@ -2,19 +2,12 @@ import pygame
 import time
 import gladosMove as g
 
-# Initialisation de pygame
+
 pygame.init()
-
-# Initialisation du joystick
 pygame.joystick.init()
-
-# Vérifier s'il y a des joysticks disponibles
 if pygame.joystick.get_count() == 0:
     print("Aucun joystick trouvé.")
     quit()
-
-# Sélectionner le premier joystick disponible
-print(pygame.joystick)
 joystick1 = pygame.joystick.Joystick(0)
 joystick1.init()
 
@@ -22,19 +15,47 @@ lastx = 0
 lasty = 0
 lastz = 0
 
+dernier_temps = time.time()
+
+def temps_ecoule():
+    global dernier_temps
+    temps_actuel = time.time()
+    temps_passe = temps_actuel - dernier_temps
+    dernier_temps = temps_actuel
+    return temps_passe
+
+
+def ecrire_valeurs_fichier(axe_1, axe_2, axe_3, temps, nom_fichier):
+    try:
+        with open(nom_fichier, "a") as f:
+            ligne = "{} {} {} {}\n".format(axe_1, axe_2, axe_3, temps)
+            f.write(ligne)
+    except FileNotFoundError:
+        with open(nom_fichier, "w") as f:
+            ligne = "{} {} {} {}\n".format(axe_1, axe_2, axe_3, temps)
+            f.write(ligne)
+
+def lire_valeurs_fichier(nom_fichier):
+    with open(nom_fichier, "r") as f:
+        lignes = f.readlines()
+        valeurs = []
+        for ligne in lignes:
+            valeurs_ligne = ligne.strip().split()
+            valeurs.append((float(valeurs_ligne[0]), float(valeurs_ligne[1]), float(valeurs_ligne[2])))
+        return valeurs
+
+
 # Boucle principale
 running = True
 g.talk(1)
 g.talkLed()
 g.RingCol(255,100,0)
 while running:
-    # Récupérer les événements
     for event in pygame.event.get():
-        # Si l'utilisateur quitte, arrêter la boucle
         if event.type == pygame.QUIT:
             running = False
 
-    # Lire les valeurs des axes
+
     axis_x = joystick1.get_axis(0)
     axis_y = joystick1.get_axis(1)
     axis_z = -joystick1.get_axis(3)
@@ -43,26 +64,28 @@ while running:
     y = int(50 + 50*axis_y)
     z = int(50 + 50*axis_z)
 
-    # Afficher les valeurs des axes
+    if x != lastx or y != lasty or z != lastz:
+        ecrire_valeurs_fichier(x,y,z,temps_ecoule(),"1.txt")
+        time.sleep(0.1)
+
+
     if x != lastx:
         g.turn(x)
         lastx = x
         print("Axe X:", x)
-        time.sleep(0.1)
     if y != lasty:
         g.tilt(y)
         lasty = y
         print("Axe Y:", y)
-        time.sleep(0.1)
     if z != lastz:
         g.trans(z)
         lastz = z
         print("Axe Z:", z)
-        time.sleep(0.1)
 
 
     # Pause pour éviter une utilisation excessive du processeur
-    pygame.time.Clock().tick(50)
+    #pygame.time.Clock().tick(50)
+
 
 # Quitter pygame
 pygame.quit()
